1393e611570e3607252c2e4b42785b91b6dc4fb7
abdula1 <Asmaa.Abdul-Amin@jhuapl.edu>
2025-09-07 02:23:14 -0400

    first commit

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-        "Close": pred_unscaled,
-            "Daily Return": (pred_unscaled / last_close - 1) if last_close != 0 else 0,
-            "MA_10": pd.Series([*window["Close"].iloc[-9:], pred_unscaled]).mean(),
-            "MA_20": pd.Series([*window["Close"].iloc[-19:], pred_unscaled]).mean(),
-            "MA_50": pd.Series([*window["Close"].iloc[-49:], pred_unscaled]).mean(),
-        }
